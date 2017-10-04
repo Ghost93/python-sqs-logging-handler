2a76c8e25eb159f00b4edff20fb9a4f5af4ff371
Shmuel Goldfarb <goldfarb.shmuel@gmail.com>
2017-10-04 11:30:12 +0300

    add general async handler

---
+++ sqs_log_handler/asyncloghandler.py
@@ -0,0 +1,19 @@
+import Queue
+import threading
+
+
+class AsyncHandler(threading.Thread):
+    def __init__(self, handler):
+        self._handler = handler
+        self._queue = Queue.Queue()
+
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        while True:
+            record = self._queue.get(block=True)
+            self._handler.emit(record)
+
+    def emit(self, record):
+        self._queue.put(record)
